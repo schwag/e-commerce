4c48f7f7a93a3e790d0b7622e0cedf1ed827e06c
schwag <justin@myriadcore.com>
2014-06-13 09:38:42 -0400

    First Commit

---
+++ php/nav.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-m-collapse">
+				<span class="sr-only">Toggle Navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="scroll navbar-brand navLogo" data-speed="380" data-target="#home" href="#navMehome"><img src="img/logo.png" alt=""></a>
+		</div>
+		<div class="collapse navbar-collapse navbar-m-collapse ">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a class="scroll" data-speed="380" data-target="#about" href="#navMeone">About</a></li>
+				<li><a class="scroll" data-speed="380" data-target="#campaigntypes" href="#navMetwo">Campaign Types</a></li>
+				<li><a class="scroll" data-speed="380" data-target="#ourcapabilities" href="#navMethree">Our Capabilities</a></li>
+				<li><a class="scroll" data-speed="380" data-target="#ourprocess" href="#navMefour">Our Process</a></li>
+				<li><a class="scroll" data-speed="380" data-target="#casestudies" href="#navMefive">Case Studies</a></li>
+				<li><a class="scroll" data-speed="380" data-target="#contact" href="#navMesix">Contact</a></li>
+                <li><a class="scroll" href="http://www.myriadcore.com/blog/" target="_blank">Blog</a></li>
+			</ul>
+		</div>
+
+</nav>
+
